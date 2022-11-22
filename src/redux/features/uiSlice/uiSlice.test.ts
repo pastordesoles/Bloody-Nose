514b7ce0a3d1a048dc5953deceba11adf8830af2
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-22 21:56:39 +0100

    Add uiSlice

---
+++ src/redux/features/uiSlice/uiSlice.test.ts
@@ -0,0 +1,59 @@
+import { UiState } from "./types";
+import {
+  closeModalActionCreator,
+  openModalActionCreator,
+  uiReducer,
+} from "./uiSlice";
+
+describe("Given closeModalReducer", () => {
+  describe("When it recieves an initial state with showModal true and a closeModalActionCreator'", () => {
+    test("Then it should return a new state with with showModal false", () => {
+      const mockUiState: UiState = {
+        isError: false,
+        modalText: "",
+        showModal: true,
+      };
+
+      const expectedUiState: UiState = {
+        ...mockUiState,
+        showModal: false,
+      };
+
+      const newUiState = uiReducer(mockUiState, closeModalActionCreator());
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+});
+
+describe("Given openModalReducer", () => {
+  const mockUiState: UiState = {
+    isError: false,
+    modalText: "",
+    showModal: false,
+  };
+
+  describe("When it recieves an initial state and a payload isError true and the text 'Error'", () => {
+    test("Then it should return a new state with the modal's isError true and modal text 'Error'", () => {
+      const actionPayload = {
+        isError: true,
+        modalText: "Error",
+      };
+      const initialUiState: UiState = {
+        ...mockUiState,
+      };
+      const expectedUiState: UiState = {
+        showModal: true,
+        modalText: actionPayload.modalText,
+        isError: actionPayload.isError,
+      };
+
+      const newUiState = uiReducer(
+        initialUiState,
+        openModalActionCreator(actionPayload)
+      );
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+});

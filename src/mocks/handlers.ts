53e379c75074f9e53d941600526ca5f244db5d8d
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-22 21:57:40 +0100

    Add msw server and handler

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+import { UserRegisterCredentials } from "../hooks/useUser/types";
+import userRoutes from "../hooks/useUser/userRoutes";
+
+const { registerRoute, usersRoute } = userRoutes;
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const handlers = [
+  rest.post(`${apiUrl}${usersRoute}${registerRoute}`, async (req, res, ctx) => {
+    const user = await req.json<UserRegisterCredentials>();
+
+    if (user.email === "xa@vi.com") {
+      return res(
+        ctx.status(409),
+        ctx.json({ error: "User already in the database" })
+      );
+    }
+
+    return res(ctx.status(201), ctx.json({ user }));
+  }),
+];
+
+export default handlers;

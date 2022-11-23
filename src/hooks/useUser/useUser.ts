301b31b472c00968cc9943db8d8e411378556893
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-23 20:00:06 +0100

    Add login and logout hook

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,14 +1,26 @@
 import axios from "axios";
+import decodeToken from "jwt-decode";
 import { openModalActionCreator } from "../../redux/features/uiSlice/uiSlice";
+import { User } from "../../redux/features/userSlice/types";
+import {
+  loginUserActionCreator,
+  logoutUserActionCreator,
+} from "../../redux/features/userSlice/userSlice";
 import { useAppDispatch } from "../../redux/hooks";
-import { UserRegisterCredentials } from "./types";
+import {
+  CustomTokenPayload,
+  LoginResponse,
+  UserCredentials,
+  UserRegisterCredentials,
+} from "./types";
 import userRoutes from "./userRoutes";
 
 const apiUrl = process.env.REACT_APP_API_URL;
-const { registerRoute, usersRoute } = userRoutes;
+const { registerRoute, usersRoute, loginRoute } = userRoutes;
 
 const useUser = () => {
   const dispatch = useAppDispatch();
+
   const registerUser = async (userData: UserRegisterCredentials) => {
     try {
       await axios.post(`${apiUrl}${usersRoute}${registerRoute}`, {
@@ -33,7 +45,50 @@ const useUser = () => {
     }
   };
 
-  return { registerUser };
+  const loginUser = async (user: UserCredentials) => {
+    try {
+      const login = await axios.post<LoginResponse>(
+        `${apiUrl}${usersRoute}${loginRoute}`,
+        {
+          username: user.username,
+          password: user.password,
+        }
+      );
+
+      const { token } = login.data;
+
+      const tokenPayload: CustomTokenPayload = decodeToken(token);
+
+      const loggedUser: User = {
+        ...tokenPayload,
+        token,
+      };
+
+      dispatch(loginUserActionCreator(loggedUser));
+      dispatch(
+        openModalActionCreator({
+          isError: false,
+          modalText: "Login succesful",
+        })
+      );
+      localStorage.setItem("token", token);
+      return true;
+    } catch (error: unknown) {
+      dispatch(
+        openModalActionCreator({
+          isError: true,
+          modalText: "Login error",
+        })
+      );
+    }
+  };
+
+  const logoutUser = () => {
+    localStorage.setItem("token", "");
+    dispatch(logoutUserActionCreator());
+  };
+
+  return { registerUser, loginUser, logoutUser };
 };
 
 export default useUser;

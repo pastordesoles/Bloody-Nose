879fe7c3256f67a15b7a5413cb0983d314eb06f2
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-24 12:55:28 +0100

    Add Loader to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import { useAppSelector } from "../../redux/hooks";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import RegisterPage from "../../pages/RegisterPage/RegisterPage";
-import { useAppSelector } from "../../redux/hooks";
 import Toast from "../Toast/Toast";
+import Loader from "../Loader/Loader";
 
 function App() {
   const uiOptions = useAppSelector(({ ui }) => ui);
 
   return (
     <>
+      <Suspense fallback={<Loader />} />
       <Routes>
         <Route path="/" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-
       {uiOptions.showModal && !uiOptions.isError && (
         <Toast severity="success" message={uiOptions.modalText} />
       )}

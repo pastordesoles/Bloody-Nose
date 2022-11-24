01d755d8c7f2bd81c0ec18fae350b3e3c5ddb776
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-24 12:31:13 +0100

    Update store with ui reducer

--- src/mocks/store/mockInitialStore.ts
+++ src/mocks/store/mockInitialStore.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { uiReducer } from "../../redux/features/uiSlice/uiSlice";
 import { userReducer } from "../../redux/features/userSlice/userSlice";
 import { store } from "../../redux/store";
 import { mockUserStateNotLogged } from "../states/mockUserStates";
@@ -6,6 +7,7 @@ import { mockUserStateNotLogged } from "../states/mockUserStates";
 const mockInitialStore: typeof store = configureStore({
   reducer: {
     user: userReducer,
+    ui: uiReducer,
   },
   preloadedState: {
     user: mockUserStateNotLogged,

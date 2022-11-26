b50d1b3e78d063f354892962d845d6a7b2359782
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-26 11:10:24 +0100

    Add loadAllSessions handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,14 @@
 import { rest } from "msw";
+import sessionsRoutes from "../hooks/useSessions/sessionsRoutes";
 import {
   UserCredentials,
   UserRegisterCredentials,
 } from "../hooks/useUser/types";
 import userRoutes from "../hooks/useUser/userRoutes";
+import mockSessionsState from "./states/mockSessionsState";
 
 const { registerRoute, usersRoute, loginRoute } = userRoutes;
+const { sessionsRoute, listRoute } = sessionsRoutes;
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const handlers = [
@@ -33,6 +36,17 @@ const handlers = [
     }
 
     return res(ctx.status(200), ctx.json({ token: "testtoken" }));
+  }),
+
+  rest.get(`${apiUrl}${sessionsRoute}${listRoute}`, (req, res, ctx) => {
+    return res.once(
+      ctx.status(404),
+      ctx.json({ error: "Error loading all sessions" })
+    );
+  }),
+
+  rest.get(`${apiUrl}${sessionsRoute}${listRoute}`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockSessionsState));
   }),
 ];
 

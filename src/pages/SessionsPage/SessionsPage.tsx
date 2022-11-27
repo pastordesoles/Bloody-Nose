348c938fcb7bed4e93485ed9842cea746a6d1a87
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-27 19:17:33 +0100

    Include LoadMore button to sessions page

--- src/pages/SessionsPage/SessionsPage.tsx
+++ src/pages/SessionsPage/SessionsPage.tsx
@@ -1,19 +1,34 @@
 import { useEffect } from "react";
+import { Grid } from "@mui/material";
 import Header from "../../components/Header/Header";
+import LoadMore from "../../components/LoadMore/LoadMore";
 import SessionList from "../../components/SessionList/SessionList";
 import useSessions from "../../hooks/useSessions/useSessions";
+import { useAppSelector } from "../../redux/hooks";
 import SessionsPageStyled from "./SessionsPageStyled";
 
 const SessionsPage = () => {
   const { loadAllsessions } = useSessions();
+  const { currentPage, totalPages } = useAppSelector(
+    (state) => state.ui.pagination
+  );
+  const isLastPage = currentPage === totalPages;
 
   useEffect(() => {
-    loadAllsessions();
-  }, [loadAllsessions]);
+    loadAllsessions(currentPage);
+  }, [currentPage, loadAllsessions]);
   return (
     <SessionsPageStyled>
       <Header />
       <SessionList />
+      {!isLastPage && (
+        <Grid item className="loadmore">
+          <LoadMore
+            isLoading={false}
+            pagination={{ currentPage, totalPages }}
+          />
+        </Grid>
+      )}
     </SessionsPageStyled>
   );
 };
--- src/pages/SessionsPage/SessionsPageStyled.tsx
+++ src/pages/SessionsPage/SessionsPageStyled.tsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 const SessionsPageStyled = styled.main`
   height: 100%;
   background-color: #3c1212;
+
+  .loadmore {
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+  }
 `;
 
 export default SessionsPageStyled;

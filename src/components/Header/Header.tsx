3f154884055795389a2327da6e7ee86bdc0eabc0
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-27 13:10:27 +0100

    Add header and its test

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import { Typography } from "@mui/material";
+import HeaderStyled from "./HeaderStyled";
+import { NavLink } from "react-router-dom";
+import useUser from "../../hooks/useUser/useUser";
+
+const Header = (): JSX.Element => {
+  const { logoutUser } = useUser();
+  return (
+    <HeaderStyled className="menu-wrapper">
+      <header className="header">
+        <div>
+          <NavLink to={"/contacts"}>
+            <Typography component="h1" className="logo-icon">
+              Bloody Nose
+            </Typography>
+          </NavLink>
+        </div>
+        <div>
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" htmlFor="menu-btn">
+            <span className="navicon"></span>
+          </label>
+          <ul className="menu">
+            <li>
+              <NavLink to={"/sessions"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/"}>My Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/"}>Set a Session</NavLink>
+            </li>
+            <li>
+              <button onClick={logoutUser}>Log out</button>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </HeaderStyled>
+  );
+};
+
+export default Header;

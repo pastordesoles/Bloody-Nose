d76a7fc79b5ded79a9c44d9659696f0846699409
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-28 10:55:21 +0100

    Add loading to login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,14 @@
+import Loader from "../../components/Loader/Loader";
 import LoginForm from "../../components/Login/Login";
+import { useAppSelector } from "../../redux/hooks";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = () => {
+  const isLoading = useAppSelector((state) => state.ui.isLoading);
   return (
     <LoginPageStyled>
       <LoginForm />
+      {isLoading && <Loader />}
     </LoginPageStyled>
   );
 };

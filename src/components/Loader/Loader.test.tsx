4718c16cac69854d8683b9716fa66c2331a5acda
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-28 12:43:18 +0100

    Change loader test

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -1,16 +1,20 @@
-import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "../../redux/store";
+import { screen } from "@testing-library/react";
+import mockSessionsState from "../../mocks/states/mockSessionsState";
+import mockUiState from "../../mocks/states/mockUiState";
+import { mockUserStateLogged } from "../../mocks/states/mockUserStates";
+import mockStore from "../../mocks/store/mockStore";
+import { renderWithProviders } from "../../test-utils/renderWithProviders";
 import Loader from "./Loader";
 
 describe("Given a Loader component", () => {
   describe("When its rendered", () => {
     test("Then it should show an animation on the screen", () => {
-      render(
-        <Provider store={store}>
-          <Loader />
-        </Provider>
-      );
+      const store = mockStore({
+        sessionsPreloadState: mockSessionsState,
+        uiPreloadState: mockUiState,
+        userPreloadState: mockUserStateLogged,
+      });
+      renderWithProviders(<Loader />, { store });
 
       const renderedAnimation = screen.queryByRole("alert");
 
--- src/mocks/store/mockStore.ts
+++ src/mocks/store/mockStore.ts
@@ -11,6 +11,7 @@ interface MockStoreProps {
   userPreloadState?: UserState;
   sessionsPreloadState?: SessionsState;
 }
+
 const mockStore = ({
   sessionsPreloadState,
   uiPreloadState,

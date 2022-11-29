c5aaa0001374001444c86a5965fffe77c4b9461e
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-29 10:29:24 +0100

    Update useSessions to avoid duplication

--- src/hooks/useSessions/useSessions.test.ts
+++ src/hooks/useSessions/useSessions.test.ts
@@ -4,6 +4,7 @@ import mockSessionsState from "../../mocks/states/mockSessionsState";
 import mockUiState from "../../mocks/states/mockUiState";
 import mockInitialStore from "../../mocks/store/mockInitialStore";
 import {
+  addSessionsActionCreator,
   loadOneSessionActionCreator,
   loadSessionsActionCreator,
 } from "../../redux/features/sessionsSlice/sessionsSlice";
@@ -49,6 +50,36 @@ describe("Given the useSessions hook", () => {
     });
   });
 
+  describe("When its method loadMoreSessions is invoked and axios rejects it", () => {
+    test("Then dispatch should be called three times to show and hide loading and to show the modal with the message 'Error loading all sessions'", async () => {
+      const {
+        result: {
+          current: { loadMoresessions },
+        },
+      } = renderHook(() => useSessions(), {
+        wrapper: ProviderWrapper,
+      });
+
+      await loadMoresessions();
+
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        1,
+        showLoadingActionCreator()
+      );
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        2,
+        hideLoadingActionCreator()
+      );
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        3,
+        openModalActionCreator({
+          isError: true,
+          modalText: "Error loading all sessions",
+        })
+      );
+    });
+  });
+
   describe("When its method loadAllSessions is invoked", () => {
     test("Then dispatch should be called three times to show and hide loading and to load the received sessions", async () => {
       const {
@@ -72,6 +103,43 @@ describe("Given the useSessions hook", () => {
       expect(dispatchSpy).toHaveBeenNthCalledWith(
         2,
         loadSessionsActionCreator(sessions)
+      );
+
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        3,
+        loadPagesActionCreator(pagination)
+      );
+
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        4,
+        hideLoadingActionCreator()
+      );
+    });
+  });
+
+  describe("When its method loadMoreSessions is invoked", () => {
+    test("Then dispatch should be called three times to show and hide loading and to load the received sessions", async () => {
+      const {
+        result: {
+          current: { loadMoresessions },
+        },
+      } = renderHook(() => useSessions(), {
+        wrapper: ProviderWrapper,
+      });
+
+      const { sessions } = mockSessionsState;
+      const { pagination } = mockUiState;
+
+      await loadMoresessions();
+
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        1,
+        showLoadingActionCreator()
+      );
+
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        2,
+        addSessionsActionCreator(sessions)
       );
 
       expect(dispatchSpy).toHaveBeenNthCalledWith(

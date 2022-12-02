0c58e950c5d246e10457aa3fbec4337897b622fa
Xavier Sans <xavi.sans.b@gmail.com>
2022-12-02 21:53:02 +0100

    Add update page to app

--- src/pages/CreateSessionPage/CreateSessionPage.tsx
+++ src/pages/CreateSessionPage/CreateSessionPage.tsx
@@ -11,7 +11,7 @@ const CreateSessionPage = () => {
     <>
       <Header />
       <CreateSessionPageStyled>
-        <SessionForm />
+        <SessionForm isUpdate={false} />
       </CreateSessionPageStyled>
       {isLoading && <Loader />}
     </>

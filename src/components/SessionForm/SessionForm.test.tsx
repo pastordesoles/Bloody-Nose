00e3a3ae5f0507dd74071497e16be1624c26148e
Xavier Sans <xavi.sans.b@gmail.com>
2022-12-02 21:51:57 +0100

    Add modifications to session form for update

--- src/components/SessionForm/SessionForm.test.tsx
+++ src/components/SessionForm/SessionForm.test.tsx
@@ -4,10 +4,12 @@ import { renderWithProviders } from "../../test-utils/renderWithProviders";
 import SessionForm from "./SessionForm";
 
 const mockCreation = jest.fn();
+const mockUpdate = jest.fn();
 
 jest.mock("../../hooks/useSessions/useSessions", () => {
   return () => ({
     addOneSession: mockCreation,
+    updateOneSession: mockUpdate,
   });
 });
 
@@ -19,7 +21,7 @@ describe("Given a Session form component", () => {
 
       const nameButton = "CREATE";
 
-      renderWithProviders(<SessionForm />);
+      renderWithProviders(<SessionForm isUpdate={false} />);
 
       const buttonCreate = screen.getByRole("button", {
         name: nameButton,
@@ -39,7 +41,7 @@ describe("Given a Session form component", () => {
       const image = new File(["avatar"], "avatar.jpg", {
         type: "image/jpg",
       });
-      renderWithProviders(<SessionForm />);
+      renderWithProviders(<SessionForm isUpdate={false} />);
 
       const title = screen.queryByRole("textbox", {
         name: "Title",
@@ -79,6 +81,54 @@ describe("Given a Session form component", () => {
       await userEvent.click(button);
 
       expect(mockCreation).toBeCalled();
+    });
+  });
+
+  describe("When its rendered and its 'UPDATE' button is clicked", () => {
+    test("Then the form should be submitted", async () => {
+      const image = new File(["avatar"], "avatar.jpg", {
+        type: "image/jpg",
+      });
+      renderWithProviders(<SessionForm isUpdate={true} />);
+
+      const title = screen.queryByRole("textbox", {
+        name: "Title",
+      }) as HTMLInputElement;
+
+      const content = screen.queryByRole("textbox", {
+        name: "Content",
+      }) as HTMLInputElement;
+
+      const level = screen.queryByRole("textbox", {
+        name: "Level",
+      }) as HTMLInputElement;
+
+      const material = screen.queryByRole("textbox", {
+        name: "Material",
+      }) as HTMLInputElement;
+
+      const picture = screen.queryByLabelText("Picture");
+
+      await userEvent.type(title, "aaaaa");
+      await userEvent.type(content, "bbbb");
+      await userEvent.type(level, "dddd");
+      await userEvent.type(material, "fffff");
+      await userEvent.upload(picture!, image);
+
+      const button = screen.queryByRole("button", { name: "UPDATE" })!;
+
+      const selectLabel = /style/i;
+      const selectEl = await screen.findByLabelText(selectLabel);
+      expect(selectEl).toBeInTheDocument();
+      userEvent.click(selectEl);
+      const optionsPopupEl = await screen.findByRole("listbox", {
+        name: selectLabel,
+      });
+      userEvent.click(within(optionsPopupEl).getByText(/karate/i));
+
+      await userEvent.click(button);
+
+      expect(mockUpdate).toBeCalled();
     });
   });
 });

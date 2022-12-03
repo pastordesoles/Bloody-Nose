60808e016f5b865ecc42ddd0152c07ae80f65913
Xavier Sans <xavi.sans.b@gmail.com>
2022-12-03 17:34:50 +0100

    Remove non-correct filter

--- src/components/SessionList/SessionList.tsx
+++ src/components/SessionList/SessionList.tsx
@@ -1,62 +1,13 @@
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useAppSelector } from "../../redux/hooks";
 import { Grid } from "@mui/material";
 import SessionCard from "../SessionCard/SessionCard";
-import { SelectStyled, SessionListStyled } from "./SessionListStyled";
-import { useMemo, useState } from "react";
+import { SessionListStyled } from "./SessionListStyled";
 
 const SessionList = (): JSX.Element => {
   const { sessions } = useAppSelector((state) => state.sessions);
 
-  const [filter, setFilter] = useState("all");
-
-  const stylesFiltered = useMemo(() => {
-    if (filter === "karate") {
-      return sessions.filter((session) => session?.style === "karate");
-    }
-
-    if (filter === "boxing") {
-      return sessions.filter((session) => session?.style === "boxing");
-    }
-
-    if (filter === "mma") {
-      return sessions.filter((session) => session?.style === "mma");
-    }
-
-    if (filter === "kickboxing") {
-      return sessions.filter((session) => session?.style === "kickboxing");
-    }
-
-    return sessions;
-  }, [filter, sessions]);
-
-  const handleFilterChange = (event: SelectChangeEvent) => {
-    setFilter(event.target.value);
-  };
-
   return (
     <>
-      <SelectStyled sx={{ minWidth: 100 }}>
-        <FormControl fullWidth variant="filled" className="select">
-          <InputLabel id="demo-simple-select-label">Styles</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={filter}
-            label="Styles"
-            onChange={handleFilterChange}
-          >
-            <MenuItem value={"all"}>All Sessions</MenuItem>
-            <MenuItem value={"karate"}>Karate</MenuItem>
-            <MenuItem value={"mma"}>MMA</MenuItem>
-            <MenuItem value={"boxing"}>Boxing</MenuItem>
-            <MenuItem value={"kickboxing"}>Kickboxing</MenuItem>
-          </Select>
-        </FormControl>
-      </SelectStyled>
       <SessionListStyled sx={{ flexGrow: 1 }} aria-label="list container">
         <Grid
           container
@@ -64,7 +15,7 @@ const SessionList = (): JSX.Element => {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 1, sm: 8, md: 8, lg: 8, xl: 8 }}
         >
-          {stylesFiltered.map((session, index) => (
+          {sessions.map((session, index) => (
             <Grid
               item
               xs={1}

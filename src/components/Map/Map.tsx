a2aefb5bb1a8694246b5d6c9f91d4583b31bd28f
Xavier Sans <xavi.sans.b@gmail.com>
2022-12-05 22:42:54 +0100

    Add geolocalization

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 type OpenStreetMapProps = {
   center: [number, number];
@@ -7,6 +7,25 @@ type OpenStreetMapProps = {
   height: number;
   width: number;
 };
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (event) {
+      setPosition(event.latlng);
+      map.flyTo(event.latlng, map.getZoom());
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 
 const OpenStreetMap = ({
   center,
@@ -20,6 +39,7 @@ const OpenStreetMap = ({
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <LocationMarker />
       <Marker position={center}>
         <Popup>
           Hello Mireia. <br /> Easily customizable.

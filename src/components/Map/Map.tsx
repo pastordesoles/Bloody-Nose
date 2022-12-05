41f6f96c6ca7b796e78e3196b176cbf339edc3a5
Xavier Sans <xavi.sans.b@gmail.com>
2022-12-05 22:57:40 +0100

    Remove unused marker

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import LocationMarker from "../LocationMarker/LocationMarker";
 
 type OpenStreetMapProps = {
@@ -21,11 +21,6 @@ const OpenStreetMap = ({
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <LocationMarker />
-      <Marker position={center}>
-        <Popup>
-          Hello Mireia. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 };

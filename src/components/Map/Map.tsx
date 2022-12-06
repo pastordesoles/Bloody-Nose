286236dabbe83efbbc3e69bf76dc218be620b5b0
Xavier Sans <xavi.sans.b@gmail.com>
2022-12-06 17:30:18 +0100

    Fix maps and geolocation

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,6 @@
-import { useEffect } from "react";
 import "leaflet-control-geocoder/dist/Control.Geocoder.css";
-
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import LocationMarker from "../LocationMarker/LocationMarker";
-import { Geocoder, geocoders } from "leaflet-control-geocoder";
 
 type OpenStreetMapProps = {
   center: [number, number];
@@ -11,20 +8,6 @@ type OpenStreetMapProps = {
   height: number;
   width: number;
 };
-
-// const GetCoordinates = () => {
-//   const map = useMap();
-
-//   useEffect(() => {
-//     if (!map) return;
-//     new Geocoder({
-//       geocoder: new geocoders.Nominatim(),
-//       position: "topleft",
-//     }).addTo(map);
-//   }, [map]);
-
-//   return null;
-// };
 
 const OpenStreetMap = ({
   center,
@@ -38,7 +21,7 @@ const OpenStreetMap = ({
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {/* <GetCoordinates /> */}
+
       <LocationMarker />
     </MapContainer>
   );
